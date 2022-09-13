50b5a97905cf85a1ef9dc97cf3605177a05ffa01
76319003 <76319003@L815WD220002.edu.cbe.ab.ca>
2022-09-13 09:35:26 -0600

    9:35 AM

---
+++ Chapter 2/src/CRT.java
@@ -0,0 +1,26 @@
+
+public class CRT
+{
+	public static void main(String[] args)
+	{	
+		/*
+		  3.
+		    Average.Java
+		    Calculates test averages
+		    Beto Duque
+		    September 13, 2022
+		*/
+		//4. The AverageScore class displays the average set of scores.
+		  
+		//5. package gradeCalculator
+		  
+		//6. public class AverageScore
+		  
+		/*7. Source code is code typed by the programmer.
+		     Bytecode is when the code is compiled and used by the computer.*/
+		  
+		/*12. print() writes to the console. 
+		      println() makes a new line and writes to the console.*/
+		
+	}
+}
