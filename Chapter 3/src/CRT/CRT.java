475e7137cf1b1f59d933e76599ec4e926382d4a1
76319003 <76319003@L815WD220002.edu.cbe.ab.ca>
2022-09-15 09:48:52 -0600

    9:48 AM

---
+++ Chapter 3/src/CRT/CRT.java
@@ -0,0 +1,55 @@
+package CRT;
+
+public class CRT 
+{
+
+	public static void main(String[] args) 
+	{
+		/*
+		 
+		 1.cat, dog, age, name
+		   int, String, My Dog, ;
+		   
+		   identifier names cannot be named after another identifier (check 1 and 2).
+		   identifiers cannot have spaces in it.
+		   identifiers cannot be naemd semi colon because the program has already assigned it to mean something else.
+		 */
+		
+		/*
+		 
+		 2. int numBeads;
+		 numBeads = 5;
+		 
+		 int numBeads = 5;
+		  
+		 */
+		
+		/*
+		  
+		 3a. yourNumber = 13;
+		 3b. yourNumber = 11;
+		 
+		 */
+		
+		/*
+		  
+		 4a. int
+		 4b. double
+		 4c. int
+		 4d. int
+		 4e. bool
+		 4f. char
+		 
+		 */
+		
+		/*
+		  
+		 5a. primitive data types can store a single piece of data.
+		 	 abstract data types can store data and methods.
+
+		 5b. objects are variables declared with a class.
+		 	 classes are abstract data types.
+		 */
+	}
+
+}
